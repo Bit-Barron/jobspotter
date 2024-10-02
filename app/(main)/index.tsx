5edfada6fdb41453d65d26ca42526ec73aa4809c
bit-barron <bit.barron22@gmail.com>
2024-10-03 01:48:49 +0200

    clean up

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -15,30 +15,7 @@ export default function Main() {
   return (
     <View className="p-2 flex-1">
       <Text className="text-lg font-light">Hello, {user?.username}</Text>
-      <Text className="text-3xl font-semibold mt-1">Find your perfect job</Text>
-      <View className="flex-row mt-5 items-center">
-        <View className="flex-1 mr-2">
-          <Input
-            value={search}
-            onChange={(e) => setSearch(e.nativeEvent.text)}
-            placeholder="What are you looking for?"
-          />
-        </View>
-        <Button onPress={() => setSearch(search)}>
-          <Text>Search</Text>
-        </Button>
-      </View>
-      <View className="flex-row mt-4 mb-4">
-        <TouchableOpacity className="px-4 border py-2 rounded-full mr-2">
-          <Text>Full-time</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="border px-4 py-2 rounded-full mr-2">
-          <Text>Part-time</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="border px-4 py-2 rounded-full">
-          <Text>Remote</Text>
-        </TouchableOpacity>
-      </View>
+      
       <View className="mt-5 flex-row justify-between items-center p-3">
         <Text className="text-2xl">Popular Jobs</Text>
         <TouchableOpacity>
