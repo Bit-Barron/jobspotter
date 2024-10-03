71f14ece4794a578bc6b6af20c6b9ac851ea4b42
bit-barron <bit.barron22@gmail.com>
2024-10-03 04:25:47 +0200

    clean up

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -5,15 +5,21 @@ import { useUser } from "@clerk/clerk-expo";
 import { Text } from "~/components/ui/text";
 import { PopularJobs } from "~/components/pages/dashboard/popular/popular-jobs";
 import { NearbyJobs } from "~/components/pages/dashboard/nearby/nearby-jobs";
-import { useRouter } from "expo-router";
+import { useRouter, Href } from "expo-router";
 
 export default function Main() {
   const { user } = useUser();
   const router = useRouter();
+  const { } = ""
 
   return (
     <View className="p-2 flex-1">
-      <Text className="text-lg font-light p-2">Hello, {user?.username}</Text>
+      <View className="flex-row justify-between items-center">
+        <Text className="text-lg font-light p-2">Hello, {user?.username}</Text>
+        <TouchableOpacity>
+          <Text className="text-gray-600 p-2">logout</Text>
+        </TouchableOpacity>
+      </View>
 
       <View className="mt-5 flex-row justify-between items-center p-3">
         <Text className="text-2xl">Popular Jobs</Text>
@@ -22,11 +28,12 @@ export default function Main() {
         </TouchableOpacity>
       </View>
       <PopularJobs />
+
       <View className="mt-5 flex-row justify-between items-center p-3">
         <TouchableOpacity>
           <Text className="text-2xl">Nearby jobs</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => router.push("/jobs/")}>
+        <TouchableOpacity onPress={() => router.push("/jobs" as Href<"/jobs">)}>
           <Text className="text-gray-600">Show all</Text>
         </TouchableOpacity>
       </View>
