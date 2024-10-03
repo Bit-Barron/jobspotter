87f24650c08cc105656f3247c9b18f1d7f5d80ed
bit-barron <bit.barron22@gmail.com>
2024-10-03 12:51:56 +0200

    clean up

--- components/pages/container/menubar.tsx
+++ components/pages/container/menubar.tsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
-import { Briefcase, Home, Settings, User } from "lucide-react-native";
+import {
+  Briefcase,
+  Home,
+  Settings,
+  User,
+  HeartIcon,
+} from "lucide-react-native";
+import { useRouter } from "expo-router";
 
 interface MenubarProps {}
 
 const Menubar: React.FC<MenubarProps> = () => {
+  const router = useRouter();
   return (
     <View className="flex-row justify-around items-center bg-white py-2 border-t border-gray-200">
-      <TouchableOpacity className="items-center">
+      <TouchableOpacity
+        className="items-center"
+        onPress={() => router.push("/(main)/")}
+      >
         <Home size={24} color="#000" />
         <Text className="text-xs mt-1">Home</Text>
       </TouchableOpacity>
-      <TouchableOpacity className="items-center">
+      <TouchableOpacity
+        className="items-center"
+        onPress={() => router.push("/(main)/jobs")}
+      >
         <Briefcase size={24} color="#000" />
         <Text className="text-xs mt-1">Jobs</Text>
       </TouchableOpacity>
-      <TouchableOpacity className="items-center">
-        <User size={24} color="#000" />
-        <Text className="text-xs mt-1">Profile</Text>
+      <TouchableOpacity
+        className="items-center"
+        onPress={() => router.push("/(main)/")}
+      >
+        <HeartIcon size={24} color="#000" />
+        <Text className="text-xs mt-1">Favorites</Text>
       </TouchableOpacity>
       <TouchableOpacity className="items-center">
         <Settings size={24} color="#000" />
